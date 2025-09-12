d6adea7700913d90815c0ebfa1efe0c2c2f06a03
Kumarvishal22665@ <kumarvishal22665@icloud.com>
2025-09-12 14:45:19 +0530

    Add Message

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 const Home = () => {
-    return(
-        <div>
-
-            This is our home page
-
-        </div>
-    )
-}
+    return <div> THIS IS HOME PAGEStcfgyjck </div>
+    
+};
 
 export default Home;
